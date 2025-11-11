6d451eaf1f3ee93ba5acd5c0e19415fa4256abe2
begum_kucuk9 <begumkucuk9@izmir.bel.tr>
2025-11-11 14:02:46 +0300

    Hourly and Daily Components Made Horizontally Scrollable

--- web/src/components/Forecast.jsx
+++ web/src/components/Forecast.jsx
@@ -1,14 +1,15 @@
 import HourlyForecastWidget from './HourlyForecastWidget'
 import DailyForecastWidget from './DailyForecastWidget'
+import HorizontallyScrollable from './HorizontallyScrollable'
 
 const Forecast = ({title, type, data}) => {
   return (
     <div className='mt-8 select-none'>
         <div>
             <h3>{title}</h3>
-            <div className='flex items-center overflow-hidden mt-2 pt-6'>
+            <HorizontallyScrollable className='flex items-center overflow-hidden mt-2 pt-6'>
                 {data.map((data) => (
-                    <div>
+                    <div key={data.date || data.day}>
                         {type === 'hourly' ? (
                             <HourlyForecastWidget data={data}/>
                         ) : (
@@ -16,7 +17,7 @@ const Forecast = ({title, type, data}) => {
                         ) }
                     </div>
                 ))}
-            </div>
+            </HorizontallyScrollable>
         </div>
     </div>
   )
